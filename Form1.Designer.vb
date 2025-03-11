4f549182f77bc23999df86d85b48ee0a608add0e
王双乔 <wangshuangqiao@live.com>
2025-03-11 15:46:18 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(form1))
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Label1 = New Label()
+        TextBox2 = New TextBox()
+        Button2 = New Button()
+        Label2 = New Label()
+        TextBox3 = New TextBox()
+        Button3 = New Button()
+        Label3 = New Label()
+        TextBox4 = New TextBox()
+        Button4 = New Button()
+        Button5 = New Button()
+        chkAutoStart = New CheckBox()
+        CheckBox2 = New CheckBox()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(12, 132)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.ScrollBars = ScrollBars.Both
+        TextBox1.Size = New Size(481, 372)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(134, 84)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(99, 32)
+        Button1.TabIndex = 1
+        Button1.Text = "开始连接"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(19, 12)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(89, 17)
+        Label1.TabIndex = 2
+        Label1.Text = "vnt-cli.exe目录"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(114, 12)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(178, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(313, 12)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(53, 22)
+        Button2.TabIndex = 4
+        Button2.Text = "查找"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(19, 44)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(85, 17)
+        Label2.TabIndex = 5
+        Label2.Text = "vnt服务器地址"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(114, 44)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(178, 23)
+        TextBox3.TabIndex = 6
+        TextBox3.Text = "vnt.wherewego.top:29872"
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(250, 84)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(96, 32)
+        Button3.TabIndex = 7
+        Button3.Text = "更新状态"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(372, 15)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(56, 17)
+        Label3.TabIndex = 8
+        Label3.Text = "组网编号"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(436, 12)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(48, 23)
+        TextBox4.TabIndex = 9
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(19, 84)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(97, 32)
+        Button4.TabIndex = 10
+        Button4.Text = "上次配置"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(359, 84)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(116, 32)
+        Button5.TabIndex = 11
+        Button5.Text = "最小化到托盘"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' chkAutoStart
+        ' 
+        chkAutoStart.AutoSize = True
+        chkAutoStart.Location = New Point(313, 44)
+        chkAutoStart.Name = "chkAutoStart"
+        chkAutoStart.Size = New Size(75, 21)
+        chkAutoStart.TabIndex = 12
+        chkAutoStart.Text = "自动启动"
+        chkAutoStart.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(409, 46)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(75, 21)
+        CheckBox2.TabIndex = 13
+        CheckBox2.Text = "自动连接"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 17F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(510, 519)
+        Controls.Add(CheckBox2)
+        Controls.Add(chkAutoStart)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(TextBox4)
+        Controls.Add(Label3)
+        Controls.Add(Button3)
+        Controls.Add(TextBox3)
+        Controls.Add(Label2)
+        Controls.Add(Button2)
+        Controls.Add(TextBox2)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "form1"
+        Text = "VntUi"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents chkAutoStart As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,346 @@
+﻿Imports System.Diagnostics
+Imports System.Threading
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms
+Imports System.Security.Principal
+Imports System.IO
+Imports System.Configuration
+
+
+Public Class form1
+    Inherits Form
+    'Dim process As Process
+    Dim outputThread As Thread
+
+    Private settingsPath As String = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "settings.txt")
+    Private stdInWriter As StreamWriter
+    ' 创建 NotifyIcon 控件
+    Private WithEvents process As Process
+    Private trayIcon As New NotifyIcon()
+    Private contextMenuStrip As New ContextMenuStrip()
+
+
+
+
+
+
+    Private Sub TrayIcon_DoubleClick(sender As Object, e As EventArgs)
+        ' 双击托盘图标时显示主窗口
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    ' 其他代码...
+
+    Function IsRunningAsAdmin() As Boolean
+        Dim identity As WindowsIdentity = WindowsIdentity.GetCurrent()
+        Dim principal As New WindowsPrincipal(identity)
+        Return principal.IsInRole(WindowsBuiltInRole.Administrator)
+    End Function
+
+    Private Sub InitializeContextMenu()
+        ' 创建“显示窗口”菜单项
+        Dim showMenuItem As New ToolStripMenuItem("显示窗口")
+        AddHandler showMenuItem.Click, AddressOf ShowMenuItem_Click
+
+        ' 创建“退出”菜单项
+        Dim exitMenuItem As New ToolStripMenuItem("退出")
+        AddHandler exitMenuItem.Click, AddressOf ExitMenuItem_Click
+
+        ' 将菜单项添加到上下文菜单
+        contextMenuStrip.Items.Add(showMenuItem)
+        contextMenuStrip.Items.Add(exitMenuItem)
+
+        ' 将上下文菜单关联到托盘图标
+        trayIcon.ContextMenuStrip = contextMenuStrip
+    End Sub
+
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+        SaveSettings()
+    End Sub
+
+
+    Private Sub ShowMenuItem_Click(sender As Object, e As EventArgs)
+        ' 显示或激活主窗体
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    Private Sub ExitMenuItem_Click(sender As Object, e As EventArgs)
+        ' 退出应用程序
+        trayIcon.Visible = False
+        ' Close a.exe when the form is closing
+
+        Application.Exit()
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        InitializeContextMenu()
+
+        LoadSettings()
+
+        ' 设置窗体无边框样式，这样用户就不能调整大小了
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+
+        ' 禁用最大化按钮和最小化按钮
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+
+        ' 初始化 NotifyIcon
+        trayIcon.Icon = My.Forms.form1.Icon ' 设置托盘图标
+        trayIcon.Text = "VntUi" ' 设置托盘图标的文本
+        trayIcon.Visible = True ' 设置托盘图标可见
+        TextBox2.Text = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "vnt\vnt-cli.exe")
+
+        CheckAndCreateConfigFile()
+        ' 添加双击事件处理程序
+        AddHandler trayIcon.DoubleClick, AddressOf TrayIcon_DoubleClick
+
+        ' 添加关闭事件处理程序
+        '  AddHandler Me.FormClosing, AddressOf MainForm_FormClosing
+
+        ' 检查当前是否以管理员权限运行
+        If Not IsRunningAsAdmin() Then
+            ' 如果不是，则以管理员权限重新启动程序
+            Dim processInfo As New ProcessStartInfo With {
+                .UseShellExecute = True,
+                .WorkingDirectory = Environment.CurrentDirectory,
+                .FileName = Application.ExecutablePath,
+                .Verb = "runas"
+            }
+
+            Try
+                Process.Start(processInfo)
+            Catch ex As Exception
+                ' 处理用户点击“取消”的情况
+                MessageBox.Show("无法以管理员权限运行程序。", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+
+            ' 退出当前实例
+            Application.Exit()
+        Else
+            ' 程序以管理员权限运行，执行其他代码
+
+            ' 检查注册表中是否设置了自动启动
+
+
+
+            LoadSettings()
+            ' ...
+        End If
+        Dim autoStart As Boolean = My.Computer.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", False).GetValue("vntui") IsNot Nothing
+        ' 根据自动启动设置复选框状态
+        chkAutoStart.Checked = autoStart
+
+        ' 检查配置文件是否存在，如果不存在则创建
+        CheckAndCreateConfigFile()
+
+
+        Me.CheckBox2.Checked = ReadAutoClickSetting()
+
+
+        LoadSettings()
+    End Sub
+
+    Sub ReadOutput()
+        While Not process.StandardOutput.EndOfStream
+            Dim line As String = process.StandardOutput.ReadLine()
+            Console.WriteLine(line)
+            Me.Invoke(Sub() TextBox1.AppendText(line + vbCrLf))
+        End While
+    End Sub
+
+    Sub SendCommand(command As String)
+        If process IsNot Nothing AndAlso Not process.HasExited Then
+            ' 写入命令到标准输入
+            process.StandardInput.WriteLine(command)
+            ' 刷新输入流，确保命令被发送
+            process.StandardInput.Flush()
+        End If
+    End Sub
+
+    Sub OnProcessExit(sender As Object, e As EventArgs)
+        ' 确保进程已启动
+        If Not process Is Nothing AndAlso Not process.HasExited Then
+            ' 尝试正常关闭进程
+            process.CloseMainWindow()
+            ' 等待进程退出
+            If Not process.WaitForExit(1000) Then
+                ' 如果进程没有在指定时间内退出，则强制终止
+                process.Kill()
+            End If
+            ' 释放资源
+            process.Dispose()
+        End If
+    End Sub
+    Private Sub LoadSettings()
+
+        settingsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "settings.txt")
+        If File.Exists(settingsPath) Then
+            Dim lines As String() = File.ReadAllLines(settingsPath)
+            If lines.Length >= 3 Then
+                TextBox2.Text = lines(0)
+                TextBox3.Text = lines(1)
+                TextBox4.Text = lines(2)
+            End If
+        End If
+    End Sub
+
+    Private Sub SaveSettings()
+        settingsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "settings.txt")
+        Dim lines As String() = {TextBox2.Text, TextBox3.Text, TextBox4.Text}
+        File.WriteAllLines(settingsPath, lines)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SaveSettings()
+        If process Is Nothing OrElse process.HasExited Then
+            ' Start a.exe with -aaa argument
+            process = New Process()
+            With process.StartInfo
+                .FileName = TextBox2.Text
+                .Arguments = "-s  " & TextBox3.Text & " -k " & TextBox4.Text & " --cmd" ' "-s  " & TextBox3.Text & 
+                .UseShellExecute = False
+                .CreateNoWindow = True
+                .RedirectStandardOutput = True
+                .RedirectStandardInput = True
+                .RedirectStandardError = True
+                .StandardOutputEncoding = System.Text.Encoding.UTF8
+            End With
+
+            process.Start()
+
+            ' Start a thread to read the output
+            outputThread = New Thread(AddressOf ReadOutput)
+            outputThread.Start()
+
+            ' Keep the StreamWriter to send commands later
+            stdInWriter = process.StandardInput
+        End If
+
+
+
+
+
+        SaveSettings()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Not process Is Nothing AndAlso Not process.HasExited Then
+            ' Send "list" command to a.exe
+            stdInWriter.WriteLine("list")
+            stdInWriter.Flush()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Using openFileDialog As New OpenFileDialog()
+            openFileDialog.Filter = “vnt-cli.exe|vnt-cli.exe”
+            openFileDialog.Title = “请选择 vnt-cli.exe 文件”
+
+            If openFileDialog.ShowDialog() = DialogResult.OK Then
+                TextBox2.Text = openFileDialog.FileName
+            End If
+        End Using
+
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs)
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        LoadSettings()
+    End Sub
+
+    Private Sub form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+
+        SaveSettings()
+
+        ' 确保在窗体关闭时，通知区域的图标也被移除
+        ' 确保在窗体关闭时，通知区域的图标也被移除
+        If Not trayIcon Is Nothing Then
+            trayIcon.Visible = False
+            trayIcon.Dispose()
+        End If
+
+
+        ' Close a.exe when the form is closing
+        If Not process Is Nothing AndAlso Not process.HasExited Then
+            process.Kill()
+            process.WaitForExit()
+            process.Close()
+        End If
+
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        Me.Hide() ' 隐藏主窗口
+        trayIcon.ShowBalloonTip(1000, "VntUi", "vntui已经最小化到系统托盘.", ToolTipIcon.Info) ' 显示气球提示
+
+    End Sub
+
+    Private Sub chkAutoStart_CheckedChanged(sender As Object, e As EventArgs) Handles chkAutoStart.CheckedChanged
+        Dim autoStart As Boolean = chkAutoStart.Checked
+        ' 打开或创建注册表项
+        Dim regKey As Microsoft.Win32.RegistryKey = My.Computer.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+        If autoStart Then
+            ' 如果复选框被选中，设置程序自动启动
+            regKey.SetValue("vntui", Application.ExecutablePath)
+        Else
+            ' 如果复选框未被选中，取消程序自动启动
+            regKey.DeleteValue("vntui", False)
+        End If
+    End Sub
+
+    Private Sub SaveAutoClickSetting(checked As Boolean)
+        Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+        config.AppSettings.Settings("AutoClickEnabled").Value = checked.ToString()
+        config.Save(ConfigurationSaveMode.Modified)
+        ConfigurationManager.RefreshSection("appSettings")
+    End Sub
+
+    Private Function ReadAutoClickSetting() As Boolean
+        On Error GoTo cuowu
+        Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+        Dim setting As String = config.AppSettings.Settings("AutoClickEnabled").Value
+        Return Boolean.Parse(setting)
+        Exit Function
+cuowu:
+        Return False
+    End Function
+
+    Private Sub CheckAndCreateConfigFile()
+        Dim configFile As String = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None).FilePath
+        If Not File.Exists(configFile) Then
+            ' 配置文件不存在，创建默认配置
+            Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+            config.AppSettings.Settings.Add("AutoClickEnabled", "False")
+            config.Save(ConfigurationSaveMode.Full)
+        End If
+    End Sub
+
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        ' 更新配置文件
+        SaveAutoClickSetting(Me.CheckBox2.Checked)
+
+        ' 如果复选框被选中，设置定时器
+        If Me.CheckBox2.Checked Then
+            Me.Invoke(New MethodInvoker(Sub() Button1_Click(Nothing, Nothing)))
+        Else
+            ' 如果复选框未选中，取消定时器
+
+        End If
+    End Sub
+
+    Private Sub form1_LostFocus(sender As Object, e As EventArgs) Handles Me.LostFocus
+
+    End Sub
+End Class
